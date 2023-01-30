35ce7b4c98a9d5ac422d7e688855ac702b09edbd
ElizavetaNoskova <wliza68@gmail.com>
2023-01-30 19:13:19 +0300

    first commit

--- src/Car.java
+++ src/Car.java
@@ -6,36 +6,36 @@ public class Car {
     int productionYear;
     String productionCountry;
 
-    public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry){
+    public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
         this.brand = brand;
         this.model = model;
         this.engineVolume = engineVolume;
         this.color = color;
         this.productionYear = productionYear;
         this.productionCountry = productionCountry;
-        if (this.brand == "" || this.brand == null ){
+        if (this.brand == "" || this.brand == null) {
             this.brand = "default";
         }
-        if (this.model == "" || this.model == null ) {
+        if (this.model == "" || this.model == null) {
             this.model = "default";
         }
-        if(this.productionCountry == "" || this.productionCountry == null){
-           this.productionCountry = "default";
+        if (this.productionCountry == "" || this.productionCountry == null) {
+            this.productionCountry = "default";
         }
-        if (this.engineVolume <= 0.0){
+        if (this.engineVolume <= 0.0) {
             this.engineVolume = 1.5;
         }
-        if (this.color ==""){
+        if (this.color == "") {
             this.color = "белый";
         }
-        if (this.productionYear <= 0){
+        if (this.productionYear <= 0) {
             this.productionYear = 2000;
         }
     }
 
     @Override
     public String toString() {
-        return brand + " " + model + ", " + productionYear +" года выпуска, " + engineVolume + "л объем двигателя, сборка - "+ productionCountry + ", "+ color + " цвет.";
+        return brand + " " + model + ", " + productionYear + " года выпуска, " + engineVolume + "л объем двигателя, сборка - " + productionCountry + ", " + color + " цвет.";
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Car car1 = new Car("Lada", "Granta", 1.7, "желтый", 2015, "Россия");
-        Car car2 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020,"Германия");
-        Car car3 = new Car("BMW", "Z8", 3.0, "черный", 2021,"Германия");
-        Car car4 = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018,"Южная Корея");
-        Car car5 = new Car("Hundai", "Avante", 1.6, "оранжевый", 2016,"Южная Корея");
-        Car car6 = new Car("", "",0, "", 0, "");
+        Car car2 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия");
+        Car car3 = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия");
+        Car car4 = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
+        Car car5 = new Car("Hundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+        Car car6 = new Car("", "", 0, "", 0, "");
         System.out.println(car1.toString());
         System.out.println(car2.toString());
         System.out.println(car3.toString());
@@ -13,4 +13,5 @@ public class Main {
         System.out.println(car5.toString());
         System.out.println(car6.toString());
     }
-    }
+}
+
